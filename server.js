1555d5ae8ff3b7b268296d39b9b439dd47a9d5f6
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2016-10-19 10:37:59 +0100

    created a server

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello peeps');
+});
+
+app.listen(3030, function() {
+   console.log('App listening on port 3030!')
+});
